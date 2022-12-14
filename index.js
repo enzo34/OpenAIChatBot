2dc6dfc28660cc105882f4c13a77051c2dee84fb
enzo34 <eclavelloux@gmail.com>
2022-12-14 16:31:38 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const { Configuration, OpenAIApi } = require("openai");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const port = 8080;
+
+// Configuration de l'API
+app.use(cors());
+app.use(bodyParser.json());
+
+
+const configuration = new Configuration({
+    apiKey: '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',
+});
+const openai = new OpenAIApi(configuration);
+
+
+// Fais une route qui me permet d'envoyer des messages au bot
+app.post('/bot', async (req, res) => {
+    // Utilisation de l'instruction await pour attendre la réponse de l'API
+    const bot = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: req.body.input,
+        max_tokens: 2200
+    });
+    
+    console.log(bot.data)
+    res.status(200).json({message: bot.data.choices[0].text});
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
